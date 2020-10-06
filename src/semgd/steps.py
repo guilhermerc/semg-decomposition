2fca9ea3627674a3e17c852f661610dc10c04936
guilhermerc <guilherme.riciolic@gmail.com>
2020-10-06 19:22:45 -0300

    Adding the function that performs the second step of the algorithm
    The second steps is the process of whitening the extended
    observations matrix x.

--- src/semgd/steps.py
+++ src/semgd/steps.py
@@ -4,3 +4,13 @@ def subtract_mean(x):
     "1. Subtract the mean from the observations x"
     for channel in x:
         channel[...] = channel - np.mean(channel)
+
+def whiten(x):
+    "2. Whiten x"
+    d, U = np.linalg.eigh(np.cov(x))
+    D = np.diag(d)
+
+    # W = UD^{-1/2}U.T
+    W = np.dot(U, np.dot(np.sqrt(np.linalg.inv(D)), U.T))
+
+    return np.dot(W, x)
