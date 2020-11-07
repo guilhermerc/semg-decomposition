84ae293e359e6cd3417c9b9be0ce24cd790e0117
guilhermerc <guilherme.riciolic@gmail.com>
2020-11-07 16:24:33 -0300

    Adding a function that performs the observations' extension

--- src/semgd/steps.py
+++ src/semgd/steps.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def extend(x, R):
+    "0. Extend the observations x by a R factor"
+    m, D_r = x.shape
+
+    # Builts the extended observations matrix ('x_tilde') by reverse slicing an
+    # auxiliary matrix ('x_aux'), which is the observations matrix ('x')
+    # appended with 'R' columns of 0s at the beginning.
+    x_aux = np.hstack((np.zeros((m, R)), x))
+    for j in range(D_r):
+        if j == 0:
+            x_tilde = np.array((x_aux.T[R::-1]).T)
+        else:
+            x_tilde = np.hstack((x_tilde, (x_aux.T[R + j:j - 1:-1]).T))
+
+    return x_tilde
+
+
 def subtract_mean(x):
     "1. Subtract the mean from the observations x"
     for channel in x:
