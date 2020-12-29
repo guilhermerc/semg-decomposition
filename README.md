b7d3409f1f9faa0e8e18ba3dd30b7eba514015b7
guilhermerc <guilherme.riciolic@gmail.com>
2020-12-29 15:24:51 -0300

    Implemeting steps 1-3 and updating progress tracking

--- README.md
+++ README.md
@@ -10,4 +10,15 @@ The following algorithm is described on Negro et al. (J. Neural Engineering,
 - [x] Whiten x
 - [ ] Initialize the matrix B to empty matrix
 - [ ] For i = 1, 2, ..., M repeat:
-  - [ ] 1. Initialize the vector w\_i(0) and w\_i(-1)
+    - [x] 1. Initialize the vector w\_i(0) and w\_i(-1)
+    - [x] 2. While |w\_i(n)^{T}w\_i(n - 1) - 1| < Tolx
+          a. Fixed point algorithm
+          w_i(n) = E{zg[w_i(n - 1)^{T}z]} - Aw_i(n - 1)
+          with A = E{g'[w_i(n - 1)^{T}z}
+          b. Orthogonalization
+          w_i(n) = w_i(n) - BB^{T}w_i(n)
+          c. Normalization
+          w_i(n) = w_i(n)/||w_i(n)||
+          d. Set n = n + 1
+    - [x] 3. End while
+    - (...)
--- src/semgd/steps.py
+++ src/semgd/steps.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from .spars_meas_func import g, g_der
+
 def extend(x, R):
     "Extend the observations x by a R factor"
     m, D_r = x.shape
@@ -36,3 +38,38 @@ def whiten(x):
     W = np.dot(U, np.dot(np.sqrt(np.linalg.inv(D)), U.T))
 
     return np.dot(W, x)
+
+def separation(z, B, Tolx, max_iter):
+    "Steps 1, 2 and 3"
+    # 1. Initialize the vector w_i(0) and w_i(-1)
+    m, D_r = z.shape
+    w_new = np.random.rand(m)
+
+    # 2. While |w_i(n)^{T}w_i(n - 1) - 1| < Tolx
+    n = 0
+    while True:
+        w_old = w_new
+
+        # a. Fixed point algorithm
+        # w_i(n) = E{zg[w_i(n - 1)^{T}z]} - Aw_i(n - 1)
+        # with A = E{g'[w_i(n - 1)^{T}z}
+        A = g_der(np.dot(w_old.T, z)).mean()
+        w_new = (z*g(np.dot(w_old.T, z))).mean(axis=1) - A*w_old
+
+        # b. Orthogonalization
+        # w_i(n) = w_i(n) - BB^{T}w_i(n)
+        w_new = w_new - np.dot(np.dot(B.T, w_new), B)
+
+        # c. Normalization
+        # w_i(n) = w_i(n)/||w_i(n)||
+        if np.linalg.norm(w_new) > 0:
+            w_new = w_new/np.linalg.norm(w_new)
+
+        # d. Set n = n + 1
+        n = n + 1
+
+        if np.linalg.norm(np.dot(w_new.T, w_old) - 1) < Tolx or n > max_iter:
+            break
+
+    # 3. End while
+    return w_new
